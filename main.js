9f4522d99fbb289483deb0e5f9ca0aa7bb31c933
warnerfleming <fleming14@usf.edu>
2024-11-03 21:59:55 -0500

    Implement async/await to fetch ticket data and handle errors

--- main.js
+++ main.js
@@ -1,1 +1,28 @@
+//Task 2
+const ticketContainer = document.getElementById(`ticketContainer`)
+const error = document.getElementById(`error`)
+
+async function fetchTicket(){
+    try {
+        const response = await fetch(`https://jsonplaceholder.typicode.com/posts`)
+   
+        if (!response.ok){
+            throw new Error(`Failed`)
+        }
+
+
+        const ticket = await response.json()
+
+        if (!tickets.length)
+            throw new Error(`Ticket unavailable`)
+
+    ticketContainer.innerHTML = ``
+    displayTickets(tickets)
+
+    }
+    catch (errorOccur){
+    error.textcontent = `Error`
+    }
+    
+}
 
